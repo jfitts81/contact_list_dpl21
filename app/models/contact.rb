df0d21f5aa75c8d058bb06c125179e199151506c
Jared Fitts <jfitts@gmail.com>
2021-03-02 20:27:26 -0700

    created comments model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ApplicationRecord
-   has_many :notes      
-    
+   has_many :notes, dependent: destroy
+   has_one: address, dependent: destroy
+
+   validates :first_name, :last_name, :email, presence:true
+   validates :email, uniqueness: true
+   validates :age, numericality: {only_integer:, true, greater_than_or_equal_to: 21}
+
+
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :contact
+  has_many :comments
 end
